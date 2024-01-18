718896608aac63c2687f976538908ddd74aad6c7
ashenafi yirgalem <ashenafiyirgalem@gmail.com>
2024-01-18 22:18:27 +0300

    intial commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,8 @@
 #include <fcntl.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and printsit
+ * it to the POSIX standard output.
  *
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
@@ -25,17 +26,19 @@ ssize_t read_textfile(const char *filename, ssize_t letters)
 	if (fd == -1)
 		return (0);
 
-	while ((nread = read(fd, buf, BUFSIZ)) > 0) {
-		if (nread < letters) {
+	while ((nread = read(fd, buf, BUFSIZ)) > 1)
+	{
+		if (nread < letters)
+		{
 			nwritten = write(STDOUT_FILENO, buf, nread);
-			if (nwritten != nread)
-				return (0);
+		if (nwritten != nread)
+			return (0);
 			return (nread);
 		}
 
 		nwritten = write(STDOUT_FILENO, buf, letters);
 		if (nwritten != letters)
-			return (0);
+		return (0);
 
 		letters -= nwritten;
 	}
